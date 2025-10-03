1bc74a6f035306b7624d494cac5ba7c7fe4a35e6
Juanjojps <jotajota.jps@gmail.com>
2025-10-03 09:05:55 +0200

    Tarea 3 subida

--- repaso2.py
+++ repaso2.py
@@ -1,7 +1,9 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QCheckBox, QWidget, QPushButton, QVBoxLayout, QLineEdit, QMenuBar, QStatusBar
+from PySide6 import QtCore
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QCheckBox, QWidget, QPushButton, QVBoxLayout, QLineEdit, QMenuBar, QStatusBar, QListWidget, QProgressBar
 from ui_repaso2 import Ui_MainWindow
+from PySide6.QtCore import QTimer
 
 class Ventana(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -10,18 +12,52 @@ class Ventana(QMainWindow, Ui_MainWindow):
 
         print("Iniciado")
 
-
+        #añadir items al comboBox y su texto principal
         self.boxRol.addItems(["Invitado", "Profe", "Alumno"])
         self.boxRol.setCurrentText("Introduzca su rol:")
 
+        #accion enviar (limpia todos los campos)
         self.enviar.clicked.connect(self.on_aceptar)
 
+        #temporizador
+        self.timer = QTimer(self)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self._tick)
+
+        self.iniciarBoton.clicked.connect(self.iniciar)
+
+        #barra
+        self.progressBar.setRange(0, 100)
+        self.progressBar.setValue(0)
+
+        #lista
+        self.lista.addItems(["Tarea 1", "Tarea 2", "Tarea 3"])
+        self.lista.itemDoubleClicked.connect(self.on_seleccionado)
+
+    def on_seleccionado(self, item):
+        print("Has seleccionado: "+item.text())
+
+
+
+    def _tick(self):
+        valor = self.progressBar.value() + 1
+        self.progressBar.setValue(valor)
+
+        if valor >= self.progressBar.maximum():
+            self.timer.stop()
+
+    def iniciar(self):
+        self.timer.start()
+
+
+    #funcion que comprueba datos
     def on_comprobar(self):
         if self.Nombre.text() == "" or not self.checkBox.isChecked():
             print("Faltan datos")
             return False
         return True
 
+    #funcion que al aceptar, si está correcto limpia los campos y si no, imprime datos incorrectos en consola
     def on_aceptar(self):
         if self.on_comprobar():
             print("Datos correctos")
--- ui_repaso2.py
+++ ui_repaso2.py
@@ -15,15 +15,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QLabel,
-    QLineEdit, QMainWindow, QMenuBar, QPushButton,
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFrame,
+    QLabel, QLineEdit, QListWidget, QListWidgetItem,
+    QMainWindow, QMenuBar, QProgressBar, QPushButton,
     QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(729, 530)
+        MainWindow.resize(759, 1013)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.label = QLabel(self.centralwidget)
@@ -33,7 +34,7 @@ class Ui_MainWindow(object):
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.verticalLayoutWidget = QWidget(self.centralwidget)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(9, 241, 661, 24))
+        self.verticalLayoutWidget.setGeometry(QRect(9, 74, 731, 391))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -42,20 +43,60 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.Nombre)
 
-        self.boxRol = QComboBox(self.centralwidget)
+        self.boxRol = QComboBox(self.verticalLayoutWidget)
         self.boxRol.setObjectName(u"boxRol")
-        self.boxRol.setGeometry(QRect(512, 290, 141, 24))
-        self.checkBox = QCheckBox(self.centralwidget)
+
+        self.verticalLayout.addWidget(self.boxRol)
+
+        self.checkBox = QCheckBox(self.verticalLayoutWidget)
         self.checkBox.setObjectName(u"checkBox")
-        self.checkBox.setGeometry(QRect(230, 430, 131, 20))
         self.checkBox.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
-        self.enviar = QPushButton(self.centralwidget)
+
+        self.verticalLayout.addWidget(self.checkBox)
+
+        self.enviar = QPushButton(self.verticalLayoutWidget)
         self.enviar.setObjectName(u"enviar")
-        self.enviar.setGeometry(QRect(510, 430, 101, 24))
+
+        self.verticalLayout.addWidget(self.enviar)
+
+        self.line = QFrame(self.centralwidget)
+        self.line.setObjectName(u"line")
+        self.line.setGeometry(QRect(0, 470, 721, 20))
+        self.line.setFrameShape(QFrame.Shape.HLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+        self.Repaso31label = QLabel(self.centralwidget)
+        self.Repaso31label.setObjectName(u"Repaso31label")
+        self.Repaso31label.setGeometry(QRect(270, 510, 201, 20))
+        self.Repaso31label.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.Repaso31label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.iniciarBoton = QPushButton(self.centralwidget)
+        self.iniciarBoton.setObjectName(u"iniciarBoton")
+        self.iniciarBoton.setGeometry(QRect(330, 590, 79, 24))
+        self.progressBar = QProgressBar(self.centralwidget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(10, 620, 701, 23))
+        self.progressBar.setValue(24)
+        self.line_2 = QFrame(self.centralwidget)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setGeometry(QRect(0, 680, 721, 20))
+        self.line_2.setFrameShape(QFrame.Shape.HLine)
+        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
+        self.Repaso32_label = QLabel(self.centralwidget)
+        self.Repaso32_label.setObjectName(u"Repaso32_label")
+        self.Repaso32_label.setGeometry(QRect(260, 710, 201, 20))
+        self.Repaso32_label.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.Repaso32_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.lista = QListWidget(self.centralwidget)
+        self.lista.setObjectName(u"lista")
+        self.lista.setGeometry(QRect(250, 750, 256, 192))
+        self.lista.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.Progreso = QLabel(self.centralwidget)
+        self.Progreso.setObjectName(u"Progreso")
+        self.Progreso.setGeometry(QRect(350, 560, 49, 16))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 729, 33))
+        self.menubar.setGeometry(QRect(0, 0, 759, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -70,9 +111,15 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Texto de la etiqueta", None))
         self.Nombre.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Introduzca su nombre", None))
+#if QT_CONFIG(accessibility)
+        self.boxRol.setAccessibleName("")
 #endif // QT_CONFIG(accessibility)
         self.boxRol.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Introduzca su rol:", None))
         self.checkBox.setText(QCoreApplication.translate("MainWindow", u"Acepto las normas:", None))
         self.enviar.setText(QCoreApplication.translate("MainWindow", u"Enviar datos", None))
+        self.Repaso31label.setText(QCoreApplication.translate("MainWindow", u"REPASO 3.1", None))
+        self.iniciarBoton.setText(QCoreApplication.translate("MainWindow", u"Iniciar", None))
+        self.Repaso32_label.setText(QCoreApplication.translate("MainWindow", u"REPASO 3.2", None))
+        self.Progreso.setText(QCoreApplication.translate("MainWindow", u"Progreso", None))
     # retranslateUi
 
